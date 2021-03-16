621dc76dcaca2c4b5099b5590d2564184f93d6c8
Sherif Attia <Sherif.Attia610@gmail.com>
2021-03-15 23:50:52 -0400

    :construction: sort trades by date and lowercase column names

--- portfolio.py
+++ portfolio.py
@@ -4,4 +4,13 @@ import yfinance
 
 class Portfolio:
     def __init__(self, transactions):
-        self._trades = pd.read_csv(transactions, usecols=["Date", "Symbol", "Quantity", "Price"], index_col="Date")
+        self.trades = pd.read_csv(
+            transactions,
+            usecols=["Date", "Symbol", "Quantity", "Price"],
+            index_col=["Date"],
+            parse_dates=["Date"],
+            dtype={"Symbol": "str", "Quantity": "float", "Price": "float"},
+        )
+        self.trades = self.trades.rename(columns={"Date": "date","Symbol": "symbol","Quantity": "quantity","Price": "price"})
+        self.trades = self.trades.sort_values(by="date")
+
