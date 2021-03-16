ec328537182bf447933220b6f43854d07852caf9
Sherif Attia <Sherif.Attia610@gmail.com>
2021-03-16 00:54:56 -0400

    :construction: convert trades to holdings

--- portfolio.py
+++ portfolio.py
@@ -4,18 +4,27 @@ import yfinance
 
 class Portfolio:
     def __init__(self, transactions):
-        self.trades = pd.read_csv(
+        self.trades = self._read_transactions(transactions)
+        self.holdings = self._generate_holdings()
+
+    def _read_transactions(self, transactions):
+        df = pd.read_csv(
             transactions,
             usecols=["Date", "Symbol", "Quantity", "Price"],
             index_col=["Date"],
             parse_dates=["Date"],
             dtype={"Symbol": "str", "Quantity": "float", "Price": "float"},
         )
-        self.trades = self.trades.rename(
-            columns={"Date": "date", "Symbol": "symbol", "Quantity": "quantity", "Price": "price"}
-        )
-        self.trades = self.trades.sort_values(by="date")
-        self.holdings = self._generate_holdings()
+        df = df.rename(columns={"Symbol": "symbol", "Quantity": "quantity", "Price": "price"})
+        df.index.name = "date"
+        df = df.sort_values(by=["date"])
+        df["cost"] = df.quantity * df.price
+        return df
 
     def _generate_holdings(self):
-        pass
+        df = pd.DataFrame(columns=["symbol", "quantity", "cost"], index=pd.DatetimeIndex(self.trades.index.unique()))
+        for date in self.trades.index.unique():
+            df.loc[date, "symbol"] = self.trades[self.trades.index == date]["symbol"][0]
+            df.loc[date, "quantity"] = self.trades.loc[date, "quantity"].sum()
+            df.loc[date, "cost"] = self.trades.loc[date, "cost"].sum()
+        return df
